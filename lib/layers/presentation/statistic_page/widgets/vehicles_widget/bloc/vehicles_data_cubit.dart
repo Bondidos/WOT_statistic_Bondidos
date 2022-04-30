7a73ea7ca0e7064da95413e66656ecacf257f263
gglbo <gglbond@gmail.com>
2022-04-30 15:21:01 +0300

    feat: vehicles sorting & filtering

--- lib/layers/presentation/statistic_page/widgets/vehicles_widget/bloc/vehicles_data_cubit.dart
+++ lib/layers/presentation/statistic_page/widgets/vehicles_widget/bloc/vehicles_data_cubit.dart
@@ -4,24 +4,83 @@ import 'package:wot_statistic/layers/presentation/statistic_page/widgets/vehicle
 import '../../../../../domain/entities/vehicles_data.dart';
 import '../../../../../domain/use_cases/load_vehicles_data.dart';
 
+const int byWins = 1;
+const int byBattles = 2;
+const int byMastery = 3;
+const int byLvl = 4;
+
 class VehiclesDataCubit extends Cubit<VehiclesDataState> {
   final LoadVehiclesData loadVehicles;
 
-  VehiclesDataCubit({required this.loadVehicles}) : super(const LoadingState()){
+  VehiclesDataCubit({required this.loadVehicles})
+      : super(const LoadingState()) {
     fetchVehiclesData();
   }
 
+  List<Vehicle> _vehicleList = [];
+  List<Vehicle> get vehicleList => _vehicleList;
+  List<Vehicle> sorted = [];
+
   void fetchVehiclesData() async {
     try {
-      final List<Vehicle> result = await loadVehicles.execute();
-      emit(LoadedDataState(vehiclesData: result));
+      _vehicleList = await loadVehicles.execute();
+      sorted = vehicleList;
+      emit(LoadedDataState(vehiclesData: _vehicleList));
     } catch (e) {
       emit(ErrorState(message: e.toString()));
     }
   }
 
-  Future<void> refreshList(){
+  Future<void> refreshList() {
     fetchVehiclesData();
     return Future.delayed(const Duration(seconds: 2));
   }
+
+  void sortByLvl() => _sortBy(sort: byLvl);
+
+  void sortByBattles() => _sortBy(sort: byBattles);
+
+  void sortByWins() => _sortBy(sort: byWins);
+
+  void sortByMastery() => _sortBy(sort: byMastery);
+
+  void filterByNation(String nation) => _filterByNation(nation: nation);
+
+  void _sortBy({required int sort}) {
+    if (vehicleList.isEmpty) {
+      emit(const ErrorState(message: 'No vehicles to show'));
+    }
+    emit(const LoadingState());
+    switch (sort) {
+      case byLvl:
+        sorted.sort((a, b) => b.battles.compareTo(a.battles));
+        sorted.sort((a, b) => b.tier.compareTo(a.tier));
+        break;
+      case byMastery:
+        sorted.sort((a, b) => b.battles.compareTo(a.battles));
+        sorted.sort((a, b) => b.markOfMastery.compareTo(a.markOfMastery));
+        break;
+      case byWins:
+        sorted.sort((a, b) => _calcWinPercent(b.battles, b.wins)
+            .compareTo(_calcWinPercent(a.battles, a.wins)));
+        break;
+      case byBattles:
+        sorted.sort((a, b) => b.battles.compareTo(a.battles));
+        break;
+    }
+    emit(LoadedDataState(vehiclesData: sorted));
+  }
+
+  void _filterByNation({required String nation}) {
+    if(nation == 'All') {
+      sorted = vehicleList;
+    } else {
+      sorted = vehicleList
+          .where((vehicle) => vehicle.nation == nation.toLowerCase())
+          .toList();
+    }
+    emit(LoadedDataState(vehiclesData: sorted));
+  }
+
+  double _calcWinPercent(int battles, int wins) => 100.0 / battles * wins;
 }
--- lib/layers/presentation/statistic_page/widgets/vehicles_widget/vehicles.dart
+++ lib/layers/presentation/statistic_page/widgets/vehicles_widget/vehicles.dart
@@ -1,12 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wot_statistic/layers/domain/entities/vehicles_data.dart';
 import 'package:wot_statistic/layers/presentation/statistic_page/widgets/vehicles_widget/bloc/vehicles_data_cubit.dart';
 import 'package:wot_statistic/layers/presentation/statistic_page/widgets/vehicles_widget/bloc/vehicles_state.dart';
 import 'package:wot_statistic/layers/presentation/statistic_page/widgets/vehicles_widget/vehicle_item_widget.dart';
 
-import '../../../../../common/theme/text_styles.dart';
+import 'package:wot_statistic/common/theme/text_styles.dart';
 
 class VehiclesWidget extends StatelessWidget {
   const VehiclesWidget({Key? key}) : super(key: key);
@@ -18,6 +16,22 @@ class VehiclesWidget extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Vehicles'),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.sort),
+            itemBuilder: (ctx) => <PopupMenuEntry>[
+              _sortMenuItem(onTap: cubit.sortByLvl, name: 'By Level'),
+              _sortMenuItem(onTap: cubit.sortByBattles, name: 'By Battles'),
+              _sortMenuItem(onTap: cubit.sortByMastery, name: 'By Mastery'),
+              _sortMenuItem(onTap: cubit.sortByWins, name: 'By Wins'),
+            ],
+          ),
+          PopupMenuButton(
+            icon: const Icon(Icons.filter_alt),
+            itemBuilder: (ctx) =>
+                _createFilterItems(onTap: cubit.filterByNation),
+          ),
+        ],
       ),
       body: BlocConsumer<VehiclesDataCubit, VehiclesDataState>(
           listener: (context, currentState) {
@@ -38,10 +52,11 @@ class VehiclesWidget extends StatelessWidget {
           builder: (ctx, state) {
             if (state is LoadedDataState) {
               return ListView.builder(
-                itemCount: state.vehiclesData.length,
+                  itemCount: state.vehiclesData.length,
                   itemBuilder: (ctx, index) {
-                return VehicleItemWidget(vehicle: state.vehiclesData[index]);
-              });
+                    return VehicleItemWidget(
+                        vehicle: state.vehiclesData[index]);
+                  });
             }
             return RefreshIndicator(
               onRefresh: () => cubit.refreshList(),
@@ -52,4 +67,55 @@ class VehiclesWidget extends StatelessWidget {
           }),
     );
   }
+
+  PopupMenuItem _sortMenuItem({
+    required Function onTap,
+    required String name,
+  }) {
+    return PopupMenuItem(
+      onTap: () => onTap(),
+      child: Text(name),
+    );
+  }
+
+  PopupMenuItem _filterMenuItem({
+    required Function onTap,
+    required String name,
+    required String asset,
+  }) {
+    return PopupMenuItem(
+      onTap: () => onTap(name),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Image.asset(asset,width: 28,height: 18,),
+          Text(name),
+        ],
+      ),
+    );
+  }
+
+  List<PopupMenuEntry> _createFilterItems({required Function onTap}) {
+    List<PopupMenuEntry> result = [];
+    for (var nationKey in nations.keys) {
+      result.add(_filterMenuItem(
+          onTap: onTap, name: nationKey, asset: nations[nationKey]!));
+    }
+    return result;
+  }
 }
+
+const Map<String, String> nations = {
+  "All": 'assets/nations/all_nations.png',
+  "Ussr": 'assets/nations/ussr.png',
+  "Usa": 'assets/nations/usa.png',
+  "China": 'assets/nations/china.png',
+  "Uk": 'assets/nations/uk.png',
+  "Czech": 'assets/nations/czech.png',
+  "Sweden": 'assets/nations/sweden.png',
+  "Poland": 'assets/nations/poland.png',
+  "Italy": 'assets/nations/italy.png',
+  "France": 'assets/nations/france.png',
+  "Japan": 'assets/nations/japan.png',
+  "Germany": 'assets/nations/germany.png',
+};
